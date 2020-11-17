f69e6975837fbe09ae45102660490ac8fc07173b
kisvan <65876227+kisvan@users.noreply.github.com>
2020-11-17 03:37:29 +0300

    Add files via upload

---
+++ Fish.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Fish.css';
+
+function Fish() {
+  
+  return (
+    <body>
+     <div className="samaki">  
+<h1>WE DEAL WITH FISH SUPPLY THROUGHOUT TANZANIA REGIONS</h1>
+<p>Here's the categorical selection of procedure to follow </p>
+    <div className="jamaa">
+<button className="jamaa" > Fish Categories</button>
+<button className="jamaa">Ordering</button>
+<button className="jamaa">Region</button>
+<button className="jamaa"> Delivaries</button>
+    </div>
+    </div>
+    </body>
+  );
+}
+
+export default Fish
